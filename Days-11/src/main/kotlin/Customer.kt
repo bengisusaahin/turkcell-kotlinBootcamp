827550c1873c6d2152dc5e25bb4874b94ff4140d
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-04-04 00:07:55 +0300

    Update Customer.kt

--- Days-11/src/main/kotlin/Customer.kt
+++ Days-11/src/main/kotlin/Customer.kt
@@ -1,6 +1,7 @@
 class Customer(val number: Int) : Employee() {
 
     override var tc: Long = 0
+        // get() = this.tc // Recursive property accessor
         get() = tcNo()
         set(value) { field = value }
 
