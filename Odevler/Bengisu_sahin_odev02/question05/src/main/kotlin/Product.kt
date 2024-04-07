8b1fd47656f5075b77ee9964ebddd117a865ef74
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-04-07 16:39:54 +0300

    Added task02

---
+++ Odevler/Bengisu_sahin_odev02/question05/src/main/kotlin/Product.kt
@@ -0,0 +1,7 @@
+open class Product (val name: String, val price: Double){
+    // It was created for applying inheritance
+    open fun printSize(){
+        println("All sizes for all genders ")
+    }
+
+}
