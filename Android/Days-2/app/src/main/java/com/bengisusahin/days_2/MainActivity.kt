66c2400db1c4593dbe0431e6fb4b79a8144d145b
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-04-21 14:55:56 +0300

    Days-2 added

---
+++ Android/Days-2/app/src/main/java/com/bengisusahin/days_2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bengisusahin.days_2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editTextEmail : EditText
+    lateinit var editTextPassword : EditText
+    lateinit var buttonLogin : Button
+    lateinit var buttonRegister : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // transparent
+        setContentView(R.layout.activity_main)
+
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonLogin = findViewById(R.id.buttonLogin)
+        buttonRegister = findViewById(R.id.buttonRegister)
+
+        // sahnenin(view(v) nin sağlı sollu padding ya da buna benzer genel ayarlarını apma fnc. )
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        editTextEmail.setText("Ali@mail.com")
+    }
+}
