37fe176d5f370409fb2799b392a5bce04d3039de
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-05-05 00:17:24 +0300

    Updated odev05

--- Odevler/Odev05/Bengisu_Sahin_Odev05/app/src/main/java/com/bengisusahin/bengisu_sahin_odev05/MainActivity.kt
+++ Odevler/Odev05/Bengisu_Sahin_Odev05/app/src/main/java/com/bengisusahin/bengisu_sahin_odev05/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.bengisusahin.bengisu_sahin_odev05
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonDetail : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        buttonDetail = findViewById(R.id.button)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        buttonDetail.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
         }
     }
 }
