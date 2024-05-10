9679c51ee5f727dec40d118a130995404cdb7c78
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-05-10 22:33:59 +0300

    updated programming algorithm

--- Bengisu_Sahin_Vize02/app/src/main/java/com/bengisusahin/bengisu_sahin_vize02/MainActivity.kt
+++ Bengisu_Sahin_Vize02/app/src/main/java/com/bengisusahin/bengisu_sahin_vize02/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bengisusahin.bengisu_sahin_vize02
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,8 +16,9 @@ import com.bengisusahin.bengisu_sahin_vize02.services.XmlService
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var plantList : List<Plant>
+    private lateinit var filteredPlant: List<Plant>
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -33,56 +35,79 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Bitki listesini almak için thread'i başlatıyoruz
-        Thread {
-            val plantList = XmlService().xmlLoad()
-            binding.buttonArama.setOnClickListener {
-                val searchText = binding.searchView.text.toString()
-                // Arama metniyle eşleşen bitkileri filter methodu sayesinde filtreledik
-                val filteredPlant = plantList.filter { plant ->
-                    plant.COMMON.contains(searchText, ignoreCase = true)
-                }
-                // count methoduyla aranan bitkiyle eşleşen bitki sayısını döndürdük
-                binding.textViewBulunan.text = "Bulunan : " + filteredPlant.count().toString()
-
-                binding.buttonDetay.setOnClickListener {
-                    if (filteredPlant.isNotEmpty()) {
-                        val intent = Intent(this, DetailActivity::class.java )
-                        intent.putExtra("detailCommon", filteredPlant[0].COMMON)
-                        intent.putExtra("detailBotanical", filteredPlant[0].BOTANICAL)
-                        intent.putExtra("detailZone", filteredPlant[0].ZONE)
-                        intent.putExtra("detailLight", filteredPlant[0].LIGHT)
-                        intent.putExtra("detailPrice", filteredPlant[0].PRICE)
-                        intent.putExtra("detailAvailability", filteredPlant[0].AVAILABILITY)
-                        startActivity(intent)
-                    } else {
-                        Toast.makeText(this, "Bir plant bulunamadı.", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
+        Thread{
+            plantList = XmlService().xmlLoad()
         }.start()
+
+
+        binding.buttonArama.setOnClickListener {
+
+        }
+        search()
+        detail()
 
     }
 
+    @SuppressLint("SetTextI18n")
+    fun search() {
+        // Kod Uı ile etkileşim ierdiği ve UI performansını etkileyecek uzun işlemler ierdiği için runOnUiThread
+        runOnUiThread {
+            binding.buttonArama.setOnClickListener {
+                runOnUiThread {
+                    val searchText = binding.searchView.query.toString()
+                    filteredPlant = plantList.filter { plant ->
+                        plant.COMMON.contains(searchText, ignoreCase = true)
+                    }
+                    // count methoduyla aranan bitkiyle eşleşen bitki sayısını döndürdük
+                    binding.textViewBulunan.text = "Bulunan : " + filteredPlant.count().toString()
+                }
+            }
+        }
+    }
 
-//    private fun search() {
-//        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//            @SuppressLint("SetTextI18n")
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                query?.let {
-//                    Thread {
-//
-//
-//
-//                    }.start()
-//                }
-//                return true
-//            }
-//
-//            override fun onQueryTextChange(newText: String?): Boolean {
-//                // Kullanıcı metin değişikliği yaptığında arama yapılmasını istemediğim için kullanmadım.
-//                return true
-//            }
-//        })
-//    }
-
+    private fun detail(){
+        binding.buttonDetay.setOnClickListener {
+            if (filteredPlant.isNotEmpty()) {
+                val intent = Intent(this, DetailActivity::class.java )
+                intent.putExtra("detailCommon", filteredPlant[0].COMMON)
+                intent.putExtra("detailBotanical", filteredPlant[0].BOTANICAL)
+                intent.putExtra("detailZone", filteredPlant[0].ZONE)
+                intent.putExtra("detailLight", filteredPlant[0].LIGHT)
+                intent.putExtra("detailPrice", filteredPlant[0].PRICE)
+                intent.putExtra("detailAvailability", filteredPlant[0].AVAILABILITY)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Bir plant bulunamadı.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+
+/*{
+                fun search() {
+        // Kod Uı ile etkileşim ierdiği ve UI performansını etkileyecek uzun işlemler ierdiği için runOnUiThread
+        runOnUiThread {
+            binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                @SuppressLint("SetTextI18n")
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    query?.let {
+                        filteredPlant = plantList.filter { plant ->
+                            plant.COMMON.contains(query, ignoreCase = true)
+                        }
+                        // count methoduyla aranan bitkiyle eşleşen bitki sayısını döndürdük
+                        binding.textViewBulunan.text = "Bulunan : " + filteredPlant.count().toString()
+                    }
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    // Kullanıcı metin değişikliği yaptığında arama yapılmasını istemediğim için kullanmadım.
+                    return true
+                }
+            })
+        }
+    }
+
+
+
+ */
