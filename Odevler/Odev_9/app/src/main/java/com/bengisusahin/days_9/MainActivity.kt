4a6879f3db0911b54fac9f2683225c7c1d79621f
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-05-20 00:09:46 +0300

    Update MainActivity.kt

--- Odevler/Odev_9/app/src/main/java/com/bengisusahin/days_9/MainActivity.kt
+++ Odevler/Odev_9/app/src/main/java/com/bengisusahin/days_9/MainActivity.kt
@@ -20,7 +20,7 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var iDummyService: IDummyService
-    lateinit var listViewProducts: ListView
+    private lateinit var listViewProducts: ListView
     lateinit var arr: MutableList<Product>
     private lateinit var productAdapters: ProductAdapters
     // to keep track of the current skip value
@@ -48,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         loadProducts()
 
         listViewProducts.setOnScrollListener(object : AbsListView.OnScrollListener {
+            // when scrolling state changes, this method triggers
             override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {
                 when (scrollState) {
                     AbsListView.OnScrollListener.SCROLL_STATE_IDLE -> {
@@ -64,6 +65,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+            // when scrolling the page this method triggers
             // visibleItemCount takes item count on the page
             // totalItemCount all item in the service
             override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int,
