9a17bc9ddbb4ac8abca0801f99fdfda361a7a9d0
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-05-21 17:24:12 +0300

    updated filter

--- Android/Vize03_calisma/app/src/main/java/com/bengisusahin/vize03_calisma/view/FilterActivity.kt
+++ Android/Vize03_calisma/app/src/main/java/com/bengisusahin/vize03_calisma/view/FilterActivity.kt
@@ -2,6 +2,8 @@ package com.bengisusahin.vize03_calisma.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -19,6 +21,10 @@ class FilterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFilterBinding
     private var selectedBloodGroup: String? = null
+
+    private val filterKey = "key"
+    private val filterValue = "value"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -36,67 +42,90 @@ class FilterActivity : AppCompatActivity() {
         }
 
         binding.buttonBack.setOnClickListener {
-            val intent = Intent()
-            val firstName = binding.editTextFirstName.text.toString()
-            val lastName = binding.editTextLastName.text.toString()
-            val age = binding.editTextAge.text.toString()
-            val bloodGroup = selectedBloodGroup
-
-            when {
-                firstName.isNotEmpty() && lastName.isEmpty() && age.isEmpty() && bloodGroup.isNullOrEmpty() -> {
-                    intent.putExtra("key", "firstName")
-                    intent.putExtra("value", firstName)
-                    setResult(RESULT_OK, intent)
-                }
-                lastName.isNotEmpty() && firstName.isEmpty() && age.isEmpty() && bloodGroup.isNullOrEmpty() -> {
-                    intent.putExtra("key", "lastName")
-                    intent.putExtra("value", lastName)
-                    setResult(RESULT_OK, intent)
-                }
-                age.isNotEmpty() && firstName.isEmpty() && lastName.isEmpty() && bloodGroup.isNullOrEmpty() -> {
-                    intent.putExtra("key", "age")
-                    intent.putExtra("value", age)
-                    setResult(RESULT_OK, intent)
-                }
-                !bloodGroup.isNullOrEmpty() && firstName.isEmpty() && lastName.isEmpty() && age.isEmpty() -> {
-                    intent.putExtra("key", "bloodGroup")
-                    intent.putExtra("value", bloodGroup)
-                    setResult(RESULT_OK, intent)
-                }
-                firstName.isEmpty() && lastName.isEmpty() && age.isEmpty() -> {
-                    //TODO kullanıcıya uyarı mesajı gösterilebilir
-                    /*setResult(RESULT_CANCELED)
-                    finish()
-                    return@setOnClickListener
-
-                     */
-                }
-                else -> {
-                    // If more than one field is entered or no field is entered
-                    Toast.makeText(this, "Please fill only one filter.", Toast.LENGTH_LONG).show()
-                    // "return@setOnClickListener" just stops the button click event and the onCreate function continues
-                    return@setOnClickListener
-                }
-            }
-            setResult(RESULT_OK, intent)
-            finish()
+            handleButtonClick()
         }
 
         binding.buttonClear.setOnClickListener {
             setResult(RESULT_CANCELED)
             finish()
         }
+        binding.editTextFirstName.addTextChangedListener(filterTextWatcher)
+        binding.editTextLastName.addTextChangedListener(filterTextWatcher)
+        binding.editTextAge.addTextChangedListener(filterTextWatcher)
+
     }
 
+    private val filterTextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            handleInputChange()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
+
+    private fun handleInputChange() {
+        val firstNameFilled = binding.editTextFirstName.text.isNotEmpty()
+        val lastNameFilled = binding.editTextLastName.text.isNotEmpty()
+        val ageFilled = binding.editTextAge.text.isNotEmpty()
+        val bloodGroupSelected = !selectedBloodGroup.isNullOrEmpty()
+
+        binding.editTextFirstName.isEnabled = !lastNameFilled && !ageFilled && !bloodGroupSelected
+        binding.editTextLastName.isEnabled = !firstNameFilled && !ageFilled && !bloodGroupSelected
+        binding.editTextAge.isEnabled = !firstNameFilled && !lastNameFilled && !bloodGroupSelected
+        binding.linearLayout.isEnabled = !firstNameFilled && !lastNameFilled && !ageFilled
+    }
+    private fun handleButtonClick() {
+        val firstName = formatName(binding.editTextFirstName.text.toString())
+        val lastName = formatName(binding.editTextLastName.text.toString())
+        val age = binding.editTextAge.text.toString()
+        val bloodGroup = selectedBloodGroup
+
+        val intent = Intent().apply {
+            when {
+                firstName.isNotEmpty() -> setFilter(this, "firstName", firstName)
+                lastName.isNotEmpty() -> setFilter(this, "lastName", lastName)
+                age.isNotEmpty() -> setFilter(this, "age", age)
+                !bloodGroup.isNullOrEmpty() -> setFilter(this, "bloodGroup", bloodGroup)
+                else -> {
+                    Toast.makeText(this@FilterActivity, "Please fill only one filter.",
+                        Toast.LENGTH_LONG).show()
+                    setResult(RESULT_CANCELED)
+                    finish()
+                    return
+                }
+            }
+        }
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
+    private fun setFilter(intent: Intent, key: String, value: String) {
+        intent.putExtra(filterKey, key)
+        intent.putExtra(filterValue, value)
+    }
+    
     private fun showPopup(view: View) {
         val popup = PopupMenu(this, view, Gravity.FILL)
         val inflater: MenuInflater = popup.menuInflater
         inflater.inflate(R.menu.filter_blood_group, popup.menu)
         popup.setOnMenuItemClickListener { menuItem: MenuItem ->
             selectedBloodGroup = menuItem.title.toString()
+            handleInputChange()
             Toast.makeText(this, "Selected: $selectedBloodGroup", Toast.LENGTH_SHORT).show()
             true
         }
         popup.show()
     }
+
+    private fun formatName(name: String): String {
+        return if (name.isNotEmpty()) {
+            name.lowercase().replaceFirstChar { it.uppercase() }
+        } else {
+            name
+        }
+    }
+
+
 }
