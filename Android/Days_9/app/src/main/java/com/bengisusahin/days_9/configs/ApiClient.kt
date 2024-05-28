39232a16860e244c55db6986626fb10bb1f05c8b
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-05-28 13:40:36 +0300

    Update ApiClient.kt

--- Android/Days_9/app/src/main/java/com/bengisusahin/days_9/configs/ApiClient.kt
+++ Android/Days_9/app/src/main/java/com/bengisusahin/days_9/configs/ApiClient.kt
@@ -8,6 +8,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ApiClient {
 
     private val Base_URL = "https://dummyjson.com/"
+    private val client = OkHttpClient
+        .Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .build()
+
     private val retrofit: Retrofit by lazy {
         Retrofit
             .Builder()
@@ -19,10 +24,7 @@ object ApiClient {
     // bunu da yapabilirsin ama dışardan erişip başlatılabilir ve null girebilir sıkıntı olabilir
     //lateinit var retrofit: Retrofit
 
-    private val client = OkHttpClient
-        .Builder()
-        .readTimeout(60, TimeUnit.SECONDS)
-        .build()
+
 
     fun getClient(): Retrofit {
 //        if (retrofit == null) {
