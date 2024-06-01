713a20580e8410b1167ee298f71f429c39b827a5
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-06-01 14:55:56 +0300

    updated view

--- Odevler/Odev_10/app/src/main/java/com/bengisusahin/odev_10/view/AddNoteActivity.kt
+++ Odevler/Odev_10/app/src/main/java/com/bengisusahin/odev_10/view/AddNoteActivity.kt
@@ -4,8 +4,12 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bengisusahin.odev_10.R
 import com.bengisusahin.odev_10.adapter.NoteAdapter
 import com.bengisusahin.odev_10.databinding.ActivityAddNoteBinding
 import com.bengisusahin.odev_10.models.Note
@@ -22,7 +26,13 @@ class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         noteService = NoteService(this)
         // get the userId from the intent that started this activity
--- Odevler/Odev_10/app/src/main/java/com/bengisusahin/odev_10/view/DetailActivity.kt
+++ Odevler/Odev_10/app/src/main/java/com/bengisusahin/odev_10/view/DetailActivity.kt
@@ -19,7 +19,13 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         noteService = NoteService(this)
 
